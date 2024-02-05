fbb19082eff80d593244a4e92fd5789923488fca
Akash T C <akashajay339@gmail.com>
2024-02-05 14:31:00 +0530

    creating the python file

---
+++ bankApp.py
@@ -0,0 +1,21 @@
+def displayAccDetail():
+    print("""             Account Details
+              a) Amount Deposit
+              b) Amount Withdrawal
+              c) Check Balance
+              d) Exit""")
+    choice=input("")
+
+    
+
+def main():
+    print("""             Customer
+                1. Customer Login
+                2. New Customer Sign inc
+                3. exit
+
+          """)
+    
+        
+    
+main()  
