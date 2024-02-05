087b9f5dc7884364a8dcedd61b158320367c1e49
Akash T C <akashajay339@gmail.com>
2024-02-05 15:35:33 +0530

    created and connected to databse and connected

--- bankApp.py
+++ bankApp.py
@@ -1,3 +1,19 @@
+import sqlite3
+
+sqliteConnection = sqlite3.connect('sql.db')
+
+class coustomer:
+    def __init__(self, name, passWord ):
+        self.name = name
+        self.passWord = passWord
+
+class bank(coustomer):
+    def __init__(self, name, passWord, balance ,Amount):
+        super().__init__(name, passWord)
+        self.balance = balance
+        self.Amount = Amount
+  
+
 def displayAccDetail():
     print("""             Account Details
               a) Amount Deposit
@@ -6,16 +22,41 @@ def displayAccDetail():
               d) Exit""")
     choice=input("")
 
-    
+def validate(custName,passWord):
+    return True
 
 def main():
+    bal=amt=0
+    
     print("""             Customer
                 1. Customer Login
                 2. New Customer Sign inc
                 3. exit
 
           """)
-    
+    choice1 = int(input("Enter your choice"))
+    if(choice1 == 1):
+        print("Enter coustomer name")
+        custName = input()
+        print("Enter your Password")
+        passWord = input()
+        if(validate(custName,passWord)):
+            print("welcome "+ custName)
+        else:print("invalid user")
+        
+        
+        bank(custName,passWord,bal,amt)
+        
+    if(choice1 == 2):
+         print("Enter coustomer name")
+         custName = input()
+         print("Enter your Password")
+         passWord = input()
+         
+         
+         if(validate(custName,passWord)):
+            print("welcome "+ custName)
+         else:print("invalid user")
         
     
-main()  
+main() 
